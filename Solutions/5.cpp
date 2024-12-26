62a784d60eb35314890b39be25e5ca8be41bef9c
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-12-26 18:36:30 +0530

    done 5

---
+++ Solutions/5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int gcd(int a, int b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int lcm(int a, int b) {
+    return a / gcd(a, b) * b;
+}
+
+int main() {
+    int N = 20;
+    int ans = 1;
+    for (int i = 1; i <= 20; ++i) ans = lcm(ans, i);
+    std::cout << ans << std::endl;
+}
