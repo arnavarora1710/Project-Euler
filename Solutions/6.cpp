1ae2b73d02d53d36b0d31be42ec3bcbe89da1baa
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-12-26 18:41:34 +0530

    done 6

---
+++ Solutions/6.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int N = 100;
+    long long sum = N * (N + 1) / 2;
+    sum *= sum;
+    long long sq = N * (N + 1) * (2 * N + 1) / 6;
+    std::cout << sum - sq;
+}
