c5948adf7502bb0c68eaf1924593b31e7b788a20
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-12-26 12:51:25 +0530

    first first commit

--- Solutions/1.cpp
+++ Solutions/1.cpp
@@ -4,7 +4,7 @@ int main() {
     int N = 1000;
     int ans = 0;
     for (int i = 1; i < N; ++i) {
-        ans += i % 3 == 0 or i % 5 == 0 ? i : 0;
+        ans += (i % 3 == 0 || i % 5 == 0) ? i : 0;
     }
-    std::cout << ans; // Answer : 233168
+    std::cout << ans << '\n'; // Answer: 233168
 }
