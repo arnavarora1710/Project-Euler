6f539aa2987575204a079bc4a92b660f5eaf21dd
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-12-26 18:30:42 +0530

    done 4

--- Solutions/4.cpp
+++ Solutions/4.cpp
@@ -1,1 +1,21 @@
 #include <iostream>
+#include <string>
+
+int main() {
+    int L = 100, R = 999;
+    auto check = [&] (int x) {
+        int y = 0;
+        for (int i = x; i; i /= 10)
+            y = y * 10 + i % 10;
+        return x == y;
+    };
+    int ans = 0;
+    for (int i = R; i >= L; --i) {
+        for (int j = i; j >= L; --j) {
+            if (ans < i * j && check(i * j)) {
+                ans = i * j;
+            }
+        }
+    }
+    std::cout << ans; // Answer: 906609
+}
