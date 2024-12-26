33d2360357a1285460abd0c214ff213002da028f
Arnav Arora <111811814+arnavarora1710@users.noreply.github.com>
2024-12-26 13:34:47 +0530

    done 3

---
+++ Solutions/3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    long long N = 600851475143;
+    long long ans;
+
+    // remove all 2s
+    while (!(N&1)) ans = 2, N >>= 1;
+    // remove all 3s
+    while (N % 3 == 0) ans = 3, N /= 3;
+
+    // if p is prime, then p = 6n +/- 1
+    for (long long i = 5; i*i <= N; i += 6) {
+        while (N % i == 0) ans = i, N /= i;
+        while (N % (i+2) == 0) ans = i+2, N /= i+2;
+    }
+    if (N > 4) ans = N;
+    std::cout << ans;
+}
